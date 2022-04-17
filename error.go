e60b60fc133400081d7dda2b9d3cad2b6575cd85
simon.mittag <simonmittag@gmail.com>
2022-04-17 15:49:49 +1000

    new error types

--- error.go
+++ error.go
@@ -5,5 +5,13 @@ const reset string = "connection reset by peer"
 const closed string = "use of closed network connection"
 const eof string = "EOF"
 const broken string = "broken pipe"
+const dialtimeout string = "i/o timeout"
 
-var connectionErrors = map[string]string{refused: refused, reset: reset, closed: closed, eof: eof, broken: broken}
+var connectionErrors = map[string]string{
+	refused:     refused,
+	reset:       reset,
+	closed:      closed,
+	eof:         eof,
+	broken:      broken,
+	dialtimeout: dialtimeout,
+}
