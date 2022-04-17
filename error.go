3569299e0af811e642ca63282b928832389a7f83
simon.mittag <simonmittag@gmail.com>
2022-04-17 15:53:57 +1000

    better error messages

--- error.go
+++ error.go
@@ -8,10 +8,10 @@ const broken string = "broken pipe"
 const dialtimeout string = "i/o timeout"
 
 var connectionErrors = map[string]string{
-	refused:     refused,
+	refused:     "connection refused while dialling",
 	reset:       reset,
 	closed:      closed,
-	eof:         eof,
-	broken:      broken,
-	dialtimeout: dialtimeout,
+	eof:         "EOF while reading response",
+	broken:      "use of broken pipe",
+	dialtimeout: "i/o timeout while dialling",
 }
