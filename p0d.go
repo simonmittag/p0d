711db6cac08d6f40ff3865b5f225748f626df56d
simon.mittag <simonmittag@gmail.com>
2022-04-19 08:28:16 +1000

    changed message for ulimit warning

--- p0d.go
+++ p0d.go
@@ -183,8 +183,7 @@ func (p *P0d) logBootstrap() {
 		msg := fmt.Sprintf("unable to determine OS open file limits")
 		log.Warn().Msg(msg)
 	} else if p.OsMaxOpenFiles <= int64(p.Config.Exec.Connections) {
-		msg := fmt.Sprintf("found OS open file limits [%d] too low, "+
-			"recommend reducing connections [%d] or changing OS behaviour with ulimit -n",
+		msg := fmt.Sprintf("found OS max open file limit [%d] too low, recommend reducing conns [%d]",
 			p.OsMaxOpenFiles,
 			p.Config.Exec.Connections)
 		log.Warn().Msg(msg)
