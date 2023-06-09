1d9042383fc7ac7eec1f483b68e5d8b1fb7dc6a0
simon.mittag <simonmittag@gmail.com>
2023-06-09 11:40:53 +1000

    added tests for multiple entry to main with clear flag

--- cmd/p0d/main_test.go
+++ cmd/p0d/main_test.go
@@ -1,12 +1,19 @@
 package main
 
 import (
+	"flag"
 	"os"
 	"testing"
 )
 
 func TestMainFunc(t *testing.T) {
 	os.Args = append([]string{"-v"}, "-v")
+	main()
+}
+
+func TestMainFuncWithCfg(t *testing.T) {
+	flag.CommandLine = flag.NewFlagSet(os.Args[0], flag.ExitOnError)
+	os.Args = append([]string{"-C"}, "-C", "../../examples/config_get_tls_http2_google.yml")
 	main()
 }
 
