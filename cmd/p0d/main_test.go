c5b0fba1264535f0d23bdaa88dc064fbfe3659c4
simon.mittag <simonmittag@gmail.com>
2023-06-09 11:48:19 +1000

    added tests for multiple entry to main with clear flag

--- cmd/p0d/main_test.go
+++ cmd/p0d/main_test.go
@@ -11,6 +11,24 @@ func TestMainFunc(t *testing.T) {
 	main()
 }
 
+func TestMainFuncWithHelp(t *testing.T) {
+	flag.CommandLine = flag.NewFlagSet(os.Args[0], flag.ContinueOnError)
+	os.Args = append([]string{"-h"}, "-h")
+	main()
+}
+
+func TestMainFuncWithSkipInet(t *testing.T) {
+	flag.CommandLine = flag.NewFlagSet(os.Args[0], flag.ContinueOnError)
+	os.Args = append([]string{"-s"}, "-s", "https://www.google.com")
+	main()
+}
+
+func TestMainFuncWithDuration(t *testing.T) {
+	flag.CommandLine = flag.NewFlagSet(os.Args[0], flag.ContinueOnError)
+	os.Args = append([]string{"-d"}, "-d", "3", "https://www.google.com")
+	main()
+}
+
 func TestMainFuncWithCfg(t *testing.T) {
 	flag.CommandLine = flag.NewFlagSet(os.Args[0], flag.ExitOnError)
 	os.Args = append([]string{"-C"}, "-C", "../../examples/config_get_tls_http2_google.yml")
